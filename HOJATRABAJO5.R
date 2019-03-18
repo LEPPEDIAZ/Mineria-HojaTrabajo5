6f1a639bc49a58403c2e4b8fa7525c10f234db56
jorge97710 <jazmitia_10@hotmail.com>
2019-03-18 11:07:30 -0600

    Terminado

--- HOJATRABAJO5.R
+++ HOJATRABAJO5.R
@@ -3,15 +3,19 @@ library(caret)
 perros5 <- read.csv("train.csv")
 porcentaje<-0.7
 datos<-perros5
+prueba <- perros5[,3:15]
+prueba[14] <- perros5[24]
+
+
+datos<-prueba
 set.seed(123)
 
 corte <- sample(nrow(datos),nrow(datos)*porcentaje)
 train<-datos[corte,]
 test<-datos[-corte,]
 
-modelo<-naiveBayes(AdoptionSpeed~.,data=train)
+modelo<-naiveBayes(as.factor(AdoptionSpeed)~Sterilized + Age,data=train)
 
-predBayes<-predict(modelo, newdata = test)
+predBayes<-predict(modelo, newdata = test[,1:13])
+table(predBayes,test$AdoptionSpeed)
 confusionMatrix(as.factor(predBayes),as.factor(test$AdoptionSpeed))
-
-
